5c68c5a9a1aa1feb6e8b6955e032e956b3c0a6b6
VR <vicktoriarasi@gmail.com>
2026-07-07 16:55:15 +0300

    sorting heroes using tests

---
+++ __tests__/heroes.test.js
@@ -0,0 +1,17 @@
+import sortHeroes from '../src/heroes.js';
+
+test('sorting characters by health', () => {
+    const heroes = [
+        {name: 'мечник', health: 10},
+        {name: 'маг', health: 100},
+        {name: 'лучник', health: 80},
+    ]
+
+    const result = sortHeroes(heroes);
+
+    expect(result).toEqual([
+        {name: 'маг', health: 100},
+        {name: 'лучник', health: 80},
+        {name: 'мечник', health: 10},
+    ])
+})
---
+++ src/heroes.js
@@ -0,0 +1,5 @@
+function sortHeroes(heroes) {
+    return heroes.sort((a, b) => b.health - a.health);
+}
+
+export default sortHeroes;
